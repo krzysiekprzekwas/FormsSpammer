66e0905864c792a94a105e17d8361e3b23c2eb73
Krynio <przekwaskrzysiek@gmail.com>
2018-01-12 01:17:26 +0100

    Simple get to hardcoded Google From

--- FormsSpammer/FormsSpammer.py
+++ FormsSpammer/FormsSpammer.py
@@ -1,1 +1,10 @@
-print("Initial commit")
+import urllib
+import urllib.request as urllib2
+from bs4 import BeautifulSoup
+
+url = "https://docs.google.com/forms/d/e/1FAIpQLSf_wC3PzH3nd832UbQqnvcfT07DcWudRalSQvDsVoJv4qGUuA/formResponse"
+	
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+
+print(soup)	
