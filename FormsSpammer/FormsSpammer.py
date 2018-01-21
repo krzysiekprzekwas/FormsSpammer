6322520b10898ff659e16d5540959c019f1fb5af
Krynio <przekwaskrzysiek@gmail.com>
2018-01-21 19:23:28 +0100

    Refractored spam sending to function

--- FormsSpammer/FormsSpammer.py
+++ FormsSpammer/FormsSpammer.py
@@ -17,62 +17,61 @@ class Entry:
    def __repr__(self):
       return str(self)
 
+def spam( count, url):
+    pp = pprint.PrettyPrinter(indent=4)
+    for x in range(0, count):
+        	
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
+        
+        
+        # Find form ID
+        name_box = soup.find('input', attrs={'name': 'fbzx'})
+        print("Form id: " + name_box['value'])
+        
+        entries = []
+        
+        # Find entry input fields
+        input = soup.find_all('input', {'name': re.compile('entry.[0-9]*$')})
+        #pp.pprint(list(map(lambda x : x['name'], input)))
+        
+        for tag in input:
+            opts = tag.parent.find_all("div", { "role" : re.compile('radio$|checkbox|option') })
+            values = set()
+            for opt in opts:
+                val = opt['aria-label'] if opt['role'] == "checkbox" else opt['data-value']
+                if val != "":
+                    values.add(val.split(",")[-1].strip())
+        
+            entries.append(Entry(tag['name'],values))
+        
+        pp.pprint(entries)
+        
+        values = []
+        for entry in entries:
+            values.append((entry.name,random.choice(list(entry.answers))))
+        
+        values = values + [('fvv' , '1'),
+        ('draftResponse ', '[null,null, ' + name_box['value'] + ']'),
+        ('pageHistory','0'),
+        ('fbzx', name_box['value'])
+        ]
+        
+        pp.pprint(values)
+        
+        data = urllib.parse.urlencode(values).encode("utf-8")
+        
+        print(data)
+        
+        # Send HTTP POST request
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        
+        # Read the response
+        html = response.read()
+        
+        soup = BeautifulSoup(html, 'html.parser')
+        name_box = soup.find('input', attrs={'class': 'freebirdFormviewerViewResponseConfirmationMessage'})
+        #print(soup.prettify())
 
-pp = pprint.PrettyPrinter(indent=4)
-
-# Test Google form
-url = "https://docs.google.com/forms/d/e/1FAIpQLSf_wC3PzH3nd832UbQqnvcfT07DcWudRalSQvDsVoJv4qGUuA/formResponse"
-	
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('input', attrs={'name': 'fbzx'})
-
-
-# Find form ID
-print("Form id: " + name_box['value'])
-
-entries = []
-
-# Find entry input fields
-input = soup.find_all('input', {'name': re.compile('entry.[0-9]*$')})
-#pp.pprint(list(map(lambda x : x['name'], input)))
-
-for tag in input:
-    opts = tag.parent.find_all("div", { "role" : re.compile('radio$|checkbox|option') })
-    values = set()
-    for opt in opts:
-        val = opt['aria-label'] if opt['role'] == "checkbox" else opt['data-value']
-        if val != "":
-            values.add(val.split(",")[-1].strip())
-
-    entries.append(Entry(tag['name'],values))
-
-pp.pprint(entries)
-
-values = []
-for entry in entries:
-    values.append((entry.name,random.choice(list(entry.answers))))
-
-values = values + [('fvv' , '1'),
-('draftResponse ', '[null,null, ' + name_box['value'] + ']'),
-('pageHistory','0'),
-('fbzx', name_box['value'])
-]
-
-pp.pprint(values)
-
-data = urllib.parse.urlencode(values).encode("utf-8")
-
-print(data)
-
-# Send HTTP POST request
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-
-# Read the response
-html = response.read()
-
-soup = BeautifulSoup(html, 'html.parser')
-name_box = soup.find('input', attrs={'class': 'freebirdFormviewerViewResponseConfirmationMessage'})
-#print(soup.prettify())
-
+spam(1000,"https://docs.google.com/forms/d/e/1FAIpQLSf_wC3PzH3nd832UbQqnvcfT07DcWudRalSQvDsVoJv4qGUuA/formResponse")
